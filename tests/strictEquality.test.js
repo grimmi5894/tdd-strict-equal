2980e0ae0adb32a03d99afbe5856c0068a91649f
Tricia Brooker <grimmi5894@gmail.com>
2021-11-02 18:14:32 -0400

    created first test with test data

--- tests/strictEquality.test.js
+++ tests/strictEquality.test.js
@@ -1,3 +1,16 @@
 const describe = require('mocha').describe
 const it = require('mocha').it
 const expect = require('chai').expect
+const strictEqual = require('../strictEquality')
+
+describe('strictEquality', () => {
+  describe('strictEqual', () => {
+    it('returns true when parameters are of same type and same value', () => {
+      const val1 = 5
+      const val2 = 5
+      const result = strictEqual(val1, val2)
+
+      expect(result).to.equal(true)
+    })
+  })
+})
